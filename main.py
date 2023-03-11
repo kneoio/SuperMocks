c039c19bfe186cfae199ec68350985ee7bde7e96
Kaira <kairat.kuliyev@swisscom.com>
2023-03-11 19:07:25 +0200

    #

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import http.server  # Our http server handler for http requests
 import socketserver  # Establish the TCP Socket connections
+import logging
 
 PORT = 9000
 
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.path = 'index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        path = self.path
+        if path.startswith('/v1/permittedUseCases'):
+            logging.info("/permittedUseCases request,\nPath: %s\nHeaders:\n%s\n\n", str(self.path), str(self.headers))
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("<b> Hello World !</b>".encode())
+            self.wfile.close()
+            return
+        elif path.startswith('/iopiopiopipo'):
+            self.send_response(441)
+            self.end_headers()
+            self.wfile.close()
+            return
+        else:
+            self.wfile.write("<b>The handler not found</b>".encode())
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.close()
+            return
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
 
 Handler = MyHttpRequestHandler
